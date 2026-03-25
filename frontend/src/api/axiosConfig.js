56b6211a8303b18a4d8ba2705abe1b7ae54c3aa5
hbsh2330 <hbsh2330@naver.com>
2026-03-25 14:50:51 +0900

    ip 변경5

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -6,9 +6,7 @@ const hostname = window.location.hostname;
 
 // 2. 접속한 주소가 localhost면 로컬 백엔드를, 아니면 AWS IP를 사용합니다.
 // 이렇게 하면 스프링이든 노드든 포트 5000번만 맞으면 똑같이 작동합니다.
-const baseURL = (hostname === 'localhost' || hostname === '127.0.0.1')
-  ? 'http://localhost:5000/api'           // 로컬 개발 환경
-  : `http://${hostname}:5000/api`;       // AWS 배포 환경 (자동으로 현재 IP 사용)
+const baseURL = '43.201.50.100'
   
   console.log("현재 API 서버 주소:", baseURL);
 
